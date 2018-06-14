8fda60e087bbf7473a4d5e7dba725936cbd4d0e4
zaimwa9 <B00549848@essec.edu>
2018-06-14 18:33:29 +0200

    hashtags with hardcode in comment

--- front/src/containers/UserInfo.js
+++ front/src/containers/UserInfo.js
@@ -5,13 +5,75 @@ import Rating from 'react-rating';
 
 import { Segment, Input, Item, Divider, Popup } from 'semantic-ui-react';
 
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 
+
 class UserInfo extends Component {
+
+  generateHashRow = (hash) => {
+    return (
+      <Item.Group>
+        <Item>
+          {hash}
+        </Item>
+      </Item.Group>
+    )
+  }
+
+  chunkHash = (hashArr, index) => {
+    if (index === 0) {
+      return (
+        _.map(_.chunk(hashArr, 4)[0], hash => {
+          return (
+            <Item.Content>
+              {`#${hash.content}`}
+            </Item.Content>
+          )
+        })
+      )
+    } else {
+      return (
+        _.map(_.chunk(hashArr, 4)[1], hash => {
+          return (
+            <Item.Content>
+              {`#${hash.content}`}
+            </Item.Content>
+          )
+        })
+      )
+    }
+  }
+
   render() {
+    const fakeHash = [
+      {key: 1, uuid: 1, content: 'drinks'},
+      {key: 2, uuid: 1, content: 'bitch'},
+      {key: 3, uuid: 1, content: 'football'},
+      {key: 4, uuid: 1, content: 'beach'},
+      {key: 4, uuid: 1, content: 'beach'},
+      {key: 4, uuid: 1, content: 'beach'},
+      {key: 4, uuid: 1, content: 'beach2'}
+    ]
     const popularity = 74;
     const rating = Math.round((popularity / 100 * 5) * 2) / 2;
+    const hashtags1 = _.map(_.chunk(fakeHash, 4)[0], hash => {
+      return (
+        <Item.Content>
+          {`#${hash.content}`}
+        </Item.Content>
+      )
+    });
 
+    const hashtags2 = _.map(_.chunk(fakeHash, 4)[1], hash => {
+      return (
+        <Item.Content>
+          {`#${hash.content}`}
+        </Item.Content>
+      )
+    })
+
+    console.log(fakeHash)
     return (
       <Segment textAlign='center'>
         <Item>
@@ -60,6 +122,19 @@ class UserInfo extends Component {
               </Item.Content>
             </Item>
           </Item.Group>
+          {fakeHash.length > 0 ? this.generateHashRow(this.chunkHash(fakeHash, 0)) : ''}
+          {fakeHash.length > 4 ? this.generateHashRow(this.chunkHash(fakeHash, 1)) : ''}
+          {/* <Item.Group>
+            <Item>
+              {hashtags1}
+            </Item>
+          </Item.Group>
+          <Item.Group>
+            <Item>
+              {hashtags2}
+            </Item>
+          </Item.Group> */}
+          YOYOYOYOYO
       </Segment>
     )
   }
