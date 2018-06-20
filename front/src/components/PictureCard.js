6458bc961f05a19a15e3b69c32c6ed77270824e3
Wadii <B00549848@essec.edu>
2018-06-20 10:17:52 +0200

    Dropzone enhanced

--- front/src/components/PictureCard.js
+++ front/src/components/PictureCard.js
@@ -8,8 +8,8 @@ class PictureCard extends Component {
     const url='https://pbs.twimg.com/profile_images/807269899819225089/1dqgaL06_400x400.jpg';
 //    <Icon size='large' color='orange' className='pictureIcon' name='remove' />
 
-    return(
-      <Item.Group >
+    return (
+      <Item.Group>
         <Item style={{justifyContent: 'center'}}>
           <Item.Image src={url} centered verticalAlign='middle' />
         </Item>
--- front/src/containers/PicturesZone.js
+++ front/src/containers/PicturesZone.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid, Segment, Item, Divider, Popup, Icon } from 'semantic-ui-react';
+import { Grid, Segment, Item, Image, Icon } from 'semantic-ui-react';
 
 import PropTypes from 'prop-types';
 
@@ -11,8 +11,25 @@ class PicturesZone extends Component {
   handleOnDrop = (event) => {
     console.log(event)
   }
+/*
+<Item.Group>
+  <Item style={{justifyContent: 'center'}}>
+  <Item.Meta>
+    <Dropzone
+    className='dropzone'
+    activeClassName='active-dropzone'
+    onDrop={this.handleOnDrop}
+    >
+    </Dropzone>
+    </Item.Meta>
+  </Item>
+</Item.Group>
+<Item.Image src={url} centered verticalAlign='middle' />
+
+*/
 
   render() {
+    const url='https://opensource.ncsa.illinois.edu/confluence/download/attachments/7930052/draganddrop.png?version=1&modificationDate=1310053949000&api=v2';
     return (
       <Segment >
         <Grid columns={5}>
@@ -21,11 +38,21 @@ class PicturesZone extends Component {
               <PictureCard />
             </Grid.Column>
             <Grid.Column style={{height:'100%'}}>
+            <Item.Group>
+              <Item style={{justifyContent: 'center'}}>
               <Dropzone
-                className='dropzone'
-                activeClassName='active-dropzone'
-                onDrop={this.handleOnDrop}
-              />
+              className='dropzone'
+              activeClassName='active-dropzone'
+              onDrop={this.handleOnDrop}
+              >
+              <div className='contentDropzone'>
+                <p className='textDropzone'>Drag your picture here or click to browse</p>
+                <Image src='/dropbox.svg' size='mini' centered/>
+              </div>
+              </Dropzone>
+              </Item>
+            </Item.Group>
+
             </Grid.Column>
             <Grid.Column>
               <PictureCard />
