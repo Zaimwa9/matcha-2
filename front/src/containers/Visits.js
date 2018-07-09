e970b005007573a34a9302157d34893b32ef4f92
Wadii <B00549848@essec.edu>
2018-07-09 23:14:44 +0200

    header visits

--- front/src/containers/Visits.js
+++ front/src/containers/Visits.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 
-import { Grid, Modal, Dropdown } from 'semantic-ui-react';
+import { Grid, Modal, Dropdown, Header } from 'semantic-ui-react';
 
 import FeedCard from '../components/FeedCard';
 
@@ -22,21 +22,11 @@ class Visits extends Component {
 
   render() {
     const fetchedProfiles = (this.props.feed) ? this.props.feed.profiles : [];
-    const sortingBy = this.props.sortBy ? this.props.sortBy.value : sortOptions[0].value;
 
     var profiles = _.filter(fetchedProfiles, profile => {
       // add completed profile
       return (profile.pictures[0]);
     })
-
-    var sortOrder;
-    if (sortingBy === 'distance') {
-      sortOrder = 'asc';
-    } else {
-      sortOrder = 'desc';
-    }
-
-    profiles = _.orderBy(profiles, [sortingBy], [sortOrder]);
 
     const cards =
       _.map(profiles, profile => {
@@ -54,37 +44,10 @@ class Visits extends Component {
         )
       })
 
-    const sortOptions = [
-      {
-        text: 'popularity',
-        value: 'popularity'
-      },
-      {
-        text: 'distance',
-        value: 'distance'
-      },
-      {
-        text: 'age',
-        value: 'age'
-      },
-      {
-        text: 'hashtags',
-        value: 'hashtags'
-      }
-    ]
-
     return (
       <Grid columns={2} stackable>
         <Grid.Column width={16}>
-          <span>
-            Sort by{' '}
-            <Dropdown
-              inline
-              options={sortOptions}
-              value={this.props.sortBy.text ? this.props.sortBy.text : sortOptions[0].value}
-              onChange={this.handleDropChange}
-            />
-          </span>
+          <Header>Latest visits</Header>
         </Grid.Column>
         {cards}
       </Grid>
